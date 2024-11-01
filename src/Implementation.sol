1f9df350d71d425c4e613fb8a319f02570f16d44
izz520 <1547471276@qq.com>
2024-11-01 17:12:03 +0800

    feat: init save

---
+++ src/Implementation.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+// import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+// import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+contract Implementation {
+    uint256 public number;
+
+    // function initialize(uint256 newNumber) public initializer {
+    //     number = newNumber;
+    // }
+}
